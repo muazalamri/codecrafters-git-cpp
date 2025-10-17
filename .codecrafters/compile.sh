50411748192ac8b5cb05558eb5061a57a7ce6d49
muazalamri <muazalamri2006@gmail.com>
2025-10-17 21:13:22 +0300

    recover run & compile

--- .codecrafters/compile.sh
+++ .codecrafters/compile.sh
@@ -1,1 +1,12 @@
-g++ src/main.cpp -o muaz_git -lz
+#!/bin/sh
+#
+# This script is used to compile your program on CodeCrafters
+#
+# This runs before .codecrafters/run.sh
+#
+# Learn more: https://codecrafters.io/program-interface
+
+set -e # Exit on failure
+
+cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
+cmake --build ./build
--- .codecrafters/run.sh
+++ .codecrafters/run.sh
@@ -1,1 +1,11 @@
-./muaz_git
+#!/bin/sh
+#
+# This script is used to run your program on CodeCrafters
+#
+# This runs after .codecrafters/compile.sh
+#
+# Learn more: https://codecrafters.io/program-interface
+
+set -e # Exit on failure
+
+exec $(dirname "$0")/build/git "$@"
