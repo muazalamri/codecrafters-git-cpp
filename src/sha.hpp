b97ad043e16240cd38a67b3abf195b3e249e0213
muazalamri <muazalamri2006@gmail.com>
2025-10-18 13:50:25 +0300

    stage 4.2

--- src/sha.hpp
+++ src/sha.hpp
@@ -2,14 +2,15 @@
 #include <iomanip>
 #include <iostream>
 #include <openssl/sha.h>
-std::string sha1(const std::string &data)
+std::string sha1(const std::string &text)
 {
-    std::string text = "hello world";
     unsigned char hash[SHA_DIGEST_LENGTH];
     SHA1(reinterpret_cast<const unsigned char *>(text.c_str()), text.size(), hash);
+
     std::ostringstream oss;
+    oss << std::hex << std::setfill('0');
     for (int i = 0; i < SHA_DIGEST_LENGTH; ++i)
-        oss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(hash[i]);
-    
+        oss << std::setw(2) << static_cast<unsigned int>(hash[i]);
+
     return oss.str();
 }
